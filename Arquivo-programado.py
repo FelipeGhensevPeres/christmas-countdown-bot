bc8e76fc6e64c47f25ca510ea23f6d6e83bc4443
Felipe <felipeghensev@gmail.com>
2025-12-01 14:18:03 -0300

    Update Arquivo-programado.py

--- Arquivo-programado.py
+++ Arquivo-programado.py
@@ -5,8 +5,8 @@ from twilio.rest import Client
 def chegando_natal():
 
 
-    sid_conta = "ACff4a8ea25e11dcdcbdc2ec838dccc257"
-    token = "f6fef2a69f7e2036932cb856009dfa29"
+    sid_conta = "SEU SID DA CONTA TWILLIO"
+    token = "SEU TOKEN DA CONTA TWILLIO"
 
     cliente = Client(sid_conta,token)
 
@@ -19,30 +19,31 @@ def chegando_natal():
 
     if dias_natal.days > 1:
        mensagem = cliente.messages.create(
-           to="+5511943436836",
-           from_="+12173975391",
+           to="+SEU NUMERO DE TELEFONE",
+           from_="+NUMERO FORNECIDO PELO TWILLIO",
            body= f'Faltam {dias_natal.days} dias para o natal chegar, quando chegar rode este codigo e eu te avisarei!')
        
     elif dias_natal.days == 1:
        mensagem = cliente.messages.create(
-           to="+5511943436836",
-           from_="+12173975391",
+           to="+SEU NUMERO DE TELEFONE",
+           from_="+NUMERO FORNECIDO PELO TWILLIO",
            body= 'FALTA 1 DIA PARA O NATAL!!!')
 
     elif dias_natal.days == 0:
         mensagem = cliente.messages.create(
-            to="+5511943436836",
-            from_="+12173975391",
+            to="+SEU NUMERO DE TELEFONE",
+            from_="+NUMERO FORNECIDO PELO TWILLIO",
             body= 'HOJE TEM NATAL AS 00:00!!!!!')
 
     else:
         mensagem = cliente.messages.create(
-            to="+5511943436836",
-            from_="+12173975391",
+            to="+SEU NUMERO DE TELEFONE",
+            from_="+NUMERO FORNECIDO PELO TWILLIO",
             body= f'O natal ja passou faz {passou_natal} dias')
            
     
 chegando_natal()
+
 
 
 
